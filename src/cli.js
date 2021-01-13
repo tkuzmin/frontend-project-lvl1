58c218f0e232da84a8e9042efb669918737f8960
Tim Kuzmin <tkuzmin@gmail.com>
2021-01-13 16:46:10 +0300

    fixed style to pass lint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () =>{
-let userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
-}
+export const greeting = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
